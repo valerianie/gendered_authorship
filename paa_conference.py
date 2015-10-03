d754f6a537ef29769fe841c69824bfb5008a5bd3
Max Callaghan <max.w.callaghan@gmail.com>
2015-10-03 19:37:27 +0200

    Updated scraper file

--- paa_conference.py
+++ paa_conference.py
@@ -8,25 +8,50 @@ link_part2 = ".princeton.edu/sessions/"
 os.chdir('/home/valeire/git_repos/gendered_authorship')
     
 f = codecs.open('paa_conference.txt', 'w', 'utf-8')
-f.write('title\tauthor1_firstn\tauthor1_name\tauthor2_firstn\tauthor2_name\tauthor3_firstn\tauthor3_name\tauthor4_firstn\tauthor4_name\tauthor5_firstn\tauthor5_name\tauthor6_firstn\tauthor6_name')
+
+titles = 'year\tsession_number\tsession_title\ttitle\tabstract'
+
+for i in range(1,8):
+    titles = titles + '\tauthor'+str(i)+'_firstname\tauthor'+str(i)+'_name'
+
+f.write(titles)
 
 for i in range(2002, 2003):
-    for j in range(1, 3):
+    conference = link_part1 + str(i) + '.princeton.edu/days'
+    page = requests.get(conference)
+    tree = html.fromstring(page.text)
+    rts = tree.xpath('//td[@class="rt"]')
+    length = len(rts)
+    for j in rts:
+        if 'P' in j.text:
+            length = length - 1
+    for j in (1,length+1):
         link = link_part1 + str(i) + link_part2 + str(j)
-        print link
         page = requests.get(link)
         tree = html.fromstring(page.text)
+        session_title = tree.xpath('//h2')[0].itertext()
+        for z in session_title:
+            s_title = z
         papers = tree.xpath('//li/p')
         for k in papers:
             paper_title = k.xpath('a')
             paper_authors = k.xpath('span')
-            title = paper_title[0].text
-            f.write('\n'+title)
-            for l in paper_authors:
-                author = l.text.split(' ',1)
-                author_firstn = author[0]
-                author_lastn =  author[1] 
-                f.write('\t'+author_firstn+'\t'+author_lastn)
+            if len(paper_titles) > 0:                
+                abstract_link = link_part1 + str(i) + ".princeton.edu" + paper_titles[0].attrib['href']
+                print abstract_link
+                abstract_page = requests.get(abstract_link)
+                abstract_tree = html.fromstring(abstract_page.text)
+                abstract_divs = abstract_tree.xpath('//div[@class="abstract"]')
+                abstract = abstract_divs[0].text.replace('"','')
+                abstract = abstract.replace('\r',' ')
+                abstract = abstract.replace('\n',' ')
+                title = paper_titles[0].text.replace('"','')
+                f.write('\n'+str(i)+'\t'+str(j)+'\t'+s_title+'\t'+title+'\t'+abstract)
+                for l in paper_authors:
+                    author = l.text.split(' ',1)
+                    author_1 = author[0]
+                    author_2 = author[1]
+                    f.write('\t'+author_1+'\t'+author_2)
           
 
 f.close()
